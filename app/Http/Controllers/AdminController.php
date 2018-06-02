68c63e3cd837dfe61d022aea7be7beda7f99a846
Mobashir Monim <mobashirmonim@gmail.com>
2018-06-02 19:44:51 +0600

    Make device_id in AttendanceController nullable

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Log;
 use App\User;
+use App\Branch;
 use Carbon\Carbon;
 
 class AdminController extends Controller
@@ -37,15 +38,48 @@ class AdminController extends Controller
     public function showAll ()
     {
         if (auth()->user()->role == 'super-admin' || auth()->user()->role == 'owner') {
-            $allUsers = User::all();
+            $users = User::all();
+            $branches = Branch::all();
+            $role = 'owner';
 
-            return view('showAll');
-        } else {
+            return view('showAll')->with('users', $users)
+            ->with('branches', $branches)
+            ->with('role', $role);
+        } elseif (auth()->user()->role == 'manager') {
+            $branch = Branch::where('id', auth()->user()->branch_id)->first();
+            $user = User::where('id', $id)->where('branch_id', auth()->user()->branch_id)->get();
+            $role = 'manager';
+
+            return view('showAll')->with('users', $users)
+            ->with('branch', $branch)
+            ->with('role', $role);
+        }else {
             return redirect('/')->with('error', 'Sorry, you are not authorized to access this!');
         }
     }
 
     public function show (Request $request, $id)
+    {
+        $user = User::where('id', $id);
+        
+    }
+
+    public function createSchedule (Request $request, $id)
+    {
+        $user = User::where('id', $id);
+    }
+
+    public function storeSchedule ()
+    {
+        
+    }
+
+    public function createUser ()
+    {
+
+    }
+
+    public function storeUser ()
     {
         
     }
--- resources/views/showAll.blade.php
+++ resources/views/showAll.blade.php
@@ -1,33 +1,72 @@
 <div class="container-fluid">
-    <div class="row border-top border-bottom">
-        <div class="col-md-4">
-            <strong>Name</strong>
-        </div>
-        <div class="col-md-3">
-            <strong>Role</strong>
-        </div>
-        <div class="col-md-2">
-            <strong>PIN</strong>
-        </div>
-        <div class="col-md-3">
-            <strong>Branch</strong>
-        </div>
-    </div>
+    @if($role == 'owner')
+        @foreach ($branches as $branch)
+            <div class="row">
+                <h3>{{$branch->name}}</h3>
+            </div>
+            <div class="row border-top border-bottom">
+                <div class="col-md-4">
+                    <strong>Name</strong>
+                </div>
+                <div class="col-md-3">
+                    <strong>Role</strong>
+                </div>
+                <div class="col-md-2">
+                    <strong>PIN</strong>
+                </div>
+                <div class="col-md-3">
+                    <strong>Branch</strong>
+                </div>
+            </div>
 
-    @foreach($users as $user)
-        <div class="row border-bottom">
+            @foreach($users as $user)
+                <div class="row border-bottom">
+                    <div class="col-md-4">
+                        <a href="/view/employee/{{$user->id}}">{{$user->name}}</a>
+                    </div>
+                    <div class="col-md-3">
+                        {{$user->role}}
+                    </div>
+                    <div class="col-md-2">
+                        {{$user->pin}}
+                    </div>
+                    <div class="col-md-3">
+                        {{$user->branch}}
+                    </div>
+                </div>
+            @endforeach
+        @endforeach
+    @else
+        <div class="row">
+            <h3>{{$branch->name}}</h3>
+        </div>
+        <div class="row border-top border-bottom">
             <div class="col-md-4">
-                <a href="/view/employee/{{$user->id}}">{{$user->name}}</a>
+                <strong>Name</strong>
             </div>
             <div class="col-md-3">
-                {{$user->role}}
-            </div>
-            <div class="col-md-2">
-                {{$user->pin}}
+                <strong>Role</strong>
             </div>
             <div class="col-md-3">
-                {{$user->branch}}
+                <strong>Branch</strong>
             </div>
         </div>
-    @endforeach
+
+        @foreach($users as $user)
+            <div class="row border-bottom">
+                <div class="col-md-4">
+                    {{$user->name}}
+                </div>
+                <div class="col-md-3">
+                    {{$user->role}}
+                </div>
+                <div class="col-md-3">
+                    {{$user->branch}}
+                </div>
+                <div class="col-md-2">
+                    <a href="/schedule/create/{{$user->id}}">Create Schedule</a>
+                </div>
+            </div>
+        @endforeach
+    @endif
 </div>
