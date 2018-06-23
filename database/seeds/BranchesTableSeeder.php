a905d1d8b524ba8b170ac8f93d30d1aa0396fc78
Mobashir Monim <mobashir@techynaf.com>
2018-06-23 13:33:37 +0600

    Update BranchTableSeeder

--- database/seeds/BranchesTableSeeder.php
+++ database/seeds/BranchesTableSeeder.php
@@ -13,7 +13,9 @@ class BranchesTableSeeder extends Seeder
     public function run()
     {
         $branches = array('Badda', 'Ascott', 'Dhanmondi', 'City Scape', 'LK Tower', 'Coffee Kiosk', 'Apollo',
-        'AISD', 'Lakeshore', 'Baridhara');
+        'AISD', 'Lakeshore', 'Baridhara', 'Accounts and Finance', 'HR and Admin', 'Marketing', 'Training',
+        'IT and creative', 'Health and Safety', 'Facilities', 'Supply Chain', 'Drivers and logistics',
+        'Cleaning', 'Pastry');
         foreach ($branches as $branch) {
             DB::table('branches')->insert([
                 'name' => $branch,
