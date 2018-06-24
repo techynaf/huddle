f7722c3afbbb0f06b0fc05bf589d40ae22ec4a18
Mobashir Monim <mobashir@techynaf.com>
2018-06-24 15:04:28 +0600

    Alter logs table seeder

--- database/seeds/LogsTableSeeder.php
+++ database/seeds/LogsTableSeeder.php
@@ -13,17 +13,6 @@ class LogsTableSeeder extends Seeder
      */
     public function run()
     {
-        $now = new Carbon;
-        $date = $now->format('Y-m-d');
-        DB::table('logs')->insert([
-            'user_id' => 2,
-            'date' => $date,
-            'punch_in_difference' => 0,
-            'punch_out_difference' => null,
-            'punch_in_approval' => null,
-            'punch_out_approval' => null,
-            'is_late' => false,
-            'branch_id' => 1,
-        ]);
+        //
     }
 }
