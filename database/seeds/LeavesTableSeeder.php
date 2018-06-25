f4734b94ab4f3325c0707fae3563b31db7a29761
Mobashir Monim <mobashir@techynaf.com>
2018-06-25 08:06:06 +0600

    Alter leaves table seeder

--- database/seeds/LeavesTableSeeder.php
+++ database/seeds/LeavesTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class LeavesTableSeeder extends Seeder
 {
@@ -12,13 +13,19 @@ class LeavesTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i=1; $i <= 201; $i++) { 
+        $now = new Carbon;
+        $date = $now->copy()->format('Y-m-d');
+        for ($i=1; $i <= 201; $i++) {
+            $d = $date->copy()->addDays(rand(0,4));
+
             DB::table('leaves')->insert([
                 'user_id' => $i,
-                'sick_leave' => 12,
-                'annual_leave' => 14,
-                'govt_holiday' => 12,
-                'year' => 2018,
+                'type' => 1,
+                'subject' => 'Default',
+                'body' => 'Allowed by NE',
+                'date' => $d,
+                'days' => 2,
+                'is_approved' => true,
             ]);
         }
     }
