827ef38eb9ebe3f928efa2163bd4590a61d7ad29
Mobashir Monim <mobashir@techynaf.com>
2018-06-25 08:02:23 +0600

    Create relationship between leave type and leave

--- app/Leave.php
+++ app/Leave.php
@@ -4,11 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\User;
+use App\LeaveType;
 
 class Leave extends Model
 {
     public function user ()
     {
         return $this->belongsTo('App\User');
+    }
+
+    public function leavetype ()
+    {
+        return $this->belongsTo('App\LeaveTypes');
     }
 }
--- app/LeaveTypes.php
+++ app/LeaveTypes.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Leave;
 
 class LeaveTypes extends Model
 {
-    //
+    public function leaves ()
+    {
+        return $this->hasMany('App\Leave');
+    }
 }
