9ad1cc99051ddc6b14f0ed9f8661ce0cbcff722c
Mobashir Monim <mobashir@techynaf.com>
2018-07-01 16:00:59 +0600

    Alter backend for showing all logged in users

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -21,56 +21,19 @@ class AdminController extends Controller
         foreach ($roles as $role) {
             $user_role = $role->name;
         }
+        $users = null;
+
+        if ($user_role == 'barista') {
+            return redirect('/dashboard')->with('error', 'You are not authorized to view this.');
+        }
 
         if ($user_role == 'manager') {
-            $log = Log::where('date', $date)->where('branch_id', auth()->user()->branch_id)->whereNull('punch_out_difference')->get();
-            $lists = array();
-
-            if (count($log) != 0) {
-                $lists = $this->listMaker($log, $lists, $date);
-            }
-
-            //Return a view with all those logged into that branch
-            return view('logged-in')->with('lists', $lists)->with('value', false);
-        } elseif (auth()->user()->roles->first() == 'admin' || $user_role == 'owner' || $user_role == 'super-admin') {
-            $log = Log::where('date', $date)->whereNull('punch_out_difference')->get();
-            $lists = array();
-
-            if (count($log) != 0) {
-                $lists = $this->listMaker($log, $lists, $date);
-            }
-
-            //Return a view with all those logged in all branch
-            return view('logged-in')->with('lists', $lists)->with('value', false);
+            $users = User::where('branch_id', auth()->user()->branch_id)->where('logged_in', true)->get();
         } else {
-            return redirect('/dashboard')->with('error', 'Sorry, you are not authorized to access this!');
+            $users = User::where('logged_in', true)->get();
         }
-    }
-
-    public function listMaker ($log, $lists, $date)
-    {
-        foreach ($log as $value) {
-            $name = $value->user->name;
-            $roles = $value->user->roles;
-            $user_role = '';
-            foreach ($roles as $role) {
-                $user_role = $role->name;
-            }
-            $schedule = Schedule::where('date', $value->date)->where('user_id', $value->user_id)->first();
-            $branch = '';
-            foreach ($log as $l) {
-                $branch = $l->branch->name;
-            }
-            $logoutTime = Carbon::parse($schedule->end)->format('H:i');
-            $sTime = Carbon::parse($schedule->start);
-
-            $diff = (-1) * $value->punch_in_difference;
-            $loginTime = $sTime->addSeconds($diff)->format('H:i');
-
-            array_push($lists, array($name, $user_role, $loginTime, $logoutTime, $date, $branch));
-        }
-
-        return $lists;
+        
+        return view('logged-in')->with('lists', $lists)->with('value', false);
     }
 
     public function showAll ()
