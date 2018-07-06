abef9dbe94fbe6b39e474ff80df3e5bd1fa07bf9
Mobashir Monim <mobashir@techynaf.com>
2018-07-06 11:08:51 +0600

    Rename scheduler view file

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -291,7 +291,7 @@ class ScheduleController extends Controller
                 array_push($schedules, $user_schedule);
             }
 
-            return view('c-schedule')->with('users', $users)->with('schedules', $schedules)->with('days', $days)->
+            return view('schedule/scheduler')->with('users', $users)->with('schedules', $schedules)->with('days', $days)->
             with('branches', $branches);
         }
     }
