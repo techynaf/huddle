245f0efa758e97a04f9b36ff9804370927523938
Mobashir Monim <mobashir@techynaf.com>
2018-07-10 09:09:17 +0600

    Restrict employee branch change to managers

--- resources/views/templates/schedule-default-form.blade.php
+++ resources/views/templates/schedule-default-form.blade.php
@@ -9,13 +9,17 @@
             </div>
         </div>
         <div class="col-sm-12">
-            <label for="entry_b[]" class="col-form-label">Entry Branch</label>
-            <select name="entry_b[]">
-                <option value="{{$user->branch->id}}">{{$user->branch->name}}</option>
-                @foreach($branches as $branch)
-                    <option value="{{$branch->id}}">{{$branch->name}}</option>
-                @endforeach
-            </select>
+            @if(auth()->user()->roles->first() == 'district-manager' || auth()->user()->roles->first() == 'super-admin')
+                <label for="entry_b[]" class="col-form-label">Entry Branch</label>
+                <select name="entry_b[]">
+                    <option value="{{$user->branch->id}}">{{$user->branch->name}}</option>
+                    @foreach($branches as $branch)
+                        <option value="{{$branch->id}}">{{$branch->name}}</option>
+                    @endforeach
+                </select>
+            @else
+                <input type="hidden" value="{{auth()->user()->branch_id}}" name="entry_b[]">
+            @endif
         </div>
         <div class="col-sm-12">
             <div class="form-group mb-0">
@@ -26,13 +30,17 @@
             </div>
         </div>
         <div class="col-sm-12">
-            <label for="exit_b[]" class="col-form-label">Exit Branch</label>
-            <select name="exit_b[]">
-                <option value="{{$user->branch->id}}">{{$user->branch->name}}</option>
-                @foreach($branches as $branch)
-                    <option value="{{$branch->id}}">{{$branch->name}}</option>
-                @endforeach
-            </select>
+            @if(auth()->user()->roles->first() == 'district-manager' || auth()->user()->roles->first() == 'super-admin')
+                <label for="exit_b[]" class="col-form-label">Exit Branch</label>
+                <select name="exit_b[]">
+                    <option value="{{$user->branch->id}}">{{$user->branch->name}}</option>
+                    @foreach($branches as $branch)
+                        <option value="{{$branch->id}}">{{$branch->name}}</option>
+                    @endforeach
+                </select>
+            @else
+                <input type="hidden" value="{{auth()->user()->branch_id}}" name="exit_b[]">
+            @endif
         </div>
     </div>
 </div>
--- resources/views/templates/schedule-form.blade.php
+++ resources/views/templates/schedule-form.blade.php
@@ -10,13 +10,17 @@
             </div>
         </div>
         <div class="col-sm-12">
-            <label for="entry_b[]" class="col-form-label">Entry Branch</label>
-            <select name="entry_b[]">
-                <option value="{{$schedule->startingBranch->id}}">{{$schedule->startingBranch->name}}</option>
-                @foreach($branches as $branch)
-                    <option value="{{$branch->id}}">{{$branch->name}}</option>
-                @endforeach
-            </select>
+            @if(auth()->user()->roles->first() == 'district-manager' || auth()->user()->roles->first() == 'super-admin')
+                <label for="entry_b[]" class="col-form-label">Entry Branch</label>
+                <select name="entry_b[]">
+                    <option value="{{$schedule->startingBranch->id}}">{{$schedule->startingBranch->name}}</option>
+                    @foreach($branches as $branch)
+                        <option value="{{$branch->id}}">{{$branch->name}}</option>
+                    @endforeach
+                </select>
+            @else
+                <input type="hidden" value="{{$schedule->startingBranch->id}}" name="entry_b[]">
+            @endif
         </div>
         <div class="col-sm-12">
             <div class="form-group mb-0">
@@ -28,13 +32,17 @@
             </div>
         </div>
         <div class="col-sm-12">
-            <label for="exit_b[]" class="col-form-label">Exit Branch</label>
-            <select name="exit_b[]">
-                <option value="{{$schedule->endingBranch->id}}">{{$schedule->endingBranch->name}}</option>
-                @foreach($branches as $branch)
-                    <option value="{{$branch->id}}">{{$branch->name}}</option>
-                @endforeach
-            </select>
+            @if(auth()->user()->roles->first() == 'district-manager' || auth()->user()->roles->first() == 'super-admin')
+                <label for="exit_b[]" class="col-form-label">Exit Branch</label>
+                <select name="exit_b[]">
+                    <option value="{{$schedule->endingBranch->id}}">{{$schedule->endingBranch->name}}</option>
+                    @foreach($branches as $branch)
+                        <option value="{{$branch->id}}">{{$branch->name}}</option>
+                    @endforeach
+                </select>
+            @else
+                <input type="hidden" value="{{$schedule->endingBranch->id}}" name="exit_b[]">
+            @endif
         </div>
     </div>
 </div>
