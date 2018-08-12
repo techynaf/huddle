d62a40224036fb07ff3b58fce379cab45dbb0a5d
Mobashir Monim <mobashir@techynaf.com>
2018-08-13 00:26:39 +0600

    Correct error on late report generation

--- resources/views/report/late.blade.php
+++ resources/views/report/late.blade.php
@@ -10,42 +10,51 @@
             <h4>{{$branch->name}}</h4>
         </div>
 
-        <div class="row">
-            <div class="col-12 card-box">
-                <div class="row">
-                    <div class="col">Name</div>
-                    <div class="col">Status</div>
-                    <div class="col">Religion</div>
-                    <div class="col">Scheduled</div>
-                    <div class="col">Actual</div>
-                    <div class="col">Late Type</div>
-                    <div class="col">Date</div>
-                    <div class="col">Duration</div>
-                    <div class="col">Comment</div>
-                    <div class="col">Signed Off by</div>
-                </div>
-            </div>
-        </div>
-
-        @foreach ($lates as $late)
-            @if ($late->user->branch_id == $branch->id)
-                <div class="row">
-                    <div class="col">{{$late->user->name}}</div>
-                    <div class="col">{{$late->user->status}}</div>
-                    <div class="col">{{$late->user->religion}}</div>
-                    <div class="col">{{date("g:i A", strtotime($late->log->schedule->start))}}</div>
-                    <div class="col">{{date("g:i A", strtotime($late->log->start))}}</div>
-                    <div class="col">{{$late->type}}</div>
-                    <div class="col">{{$late->date}}</div>
-                    <div class="col">{{$duration[$x++]}}</div>
-                    <div class="col">{{$late->comment}}</div>
-                    <div class="col">
-                        <a href="/view/employee/{{$late->alteredBy->id}}" target="_blank">
-                            {{$late->alteredBy->name}}
-                        </a>
+        <div class="card-box">
+            <div class="row">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col">Name</div>
+                        <div class="col">Status</div>
+                        <div class="col">Religion</div>
+                        <div class="col">Scheduled</div>
+                        <div class="col">Actual</div>
+                        <div class="col">Late Type</div>
+                        <div class="col">Date</div>
+                        <div class="col">Duration</div>
+                        <div class="col">Comment</div>
+                        <div class="col">Commented by</div>
                     </div>
                 </div>
-            @endif
-        @endforeach
+            </div>
+
+            <hr>
+
+            @foreach ($lates as $late)
+                @if ($late->user->branch_id == $branch->id)
+                    <div class="row">
+                        <div class="col">{{$late->user->name}}</div>
+                        <div class="col">{{$late->user->status}}</div>
+                        <div class="col">{{$late->user->religion}}</div>
+                        <div class="col">{{date("g:i A", strtotime($late->log->schedule->start))}}</div>
+                        <div class="col">{{date("g:i A", strtotime($late->log->start))}}</div>
+                        <div class="col">{{$late->type}}</div>
+                        <div class="col">{{$late->date}}</div>
+                        <div class="col">{{$duration[$x++].' mins'}}</div>
+                        <div class="col">{{$late->comment}}</div>
+                        <div class="col">
+                            @if ($late->alteredBy == null)
+                                <strong>NOT COMMENTED</strong>
+                            @else
+                                <a href="/view/employee/{{$late->alteredBy->id}}" target="_blank">
+                                    {{$late->alteredBy->name}}
+                                </a>
+                            @endif
+                        </div>
+                    </div>
+                @endif
+                <hr>
+            @endforeach
+        </div>
     @endforeach
 @endsection
