7fde7dd77dad40f19238cb40296e93ba4786aa07
Mobashir Monim <mobashir@techynaf.com>
2018-08-27 22:02:05 +0600

    Correct hours report

--- resources/views/report/hour.blade.php
+++ resources/views/report/hour.blade.php
@@ -4,34 +4,35 @@
     <div class="row">
         <h4 class="page-title">Please use Crtl + P to download</h4>
     </div>
-    @foreach($branches as $branch)
-        <section>
+    
+    <section>
+        <div class="container-fluid">
             <div class="row">
-                <h2>{{$branch->name}} Hour Report</h2>
-            </div>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12 table-responsive text-center">
-                        <table class="table table-striped table-bordered bg-white">
-                            <thead>
-                                <tr class="bg-warning">
-                                <th scope="col">Name</th>
-                                <th scope="col">Designation</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Employee Code</th>
-                                <th scope="col">Religion</th>
-            
-                                @foreach($weeks as $week)
-                                    <th scope="col">
-                                        {{date("d M", strtotime($week[0]))}} <br>
-                                        {{date("d M", strtotime($week[1]))}}
-                                    </th>
-                                @endforeach
-            
-                                <th scope="col">Total Working Hour</th>
-                                </tr>  
-                            </thead>
-                            <tbody>
+                <div class="col-12 table-responsive text-center">
+                    <table class="table table-striped table-bordered bg-white">
+                        <thead>
+                            <tr class="bg-warning">
+                            <th scope="col">Name</th>
+                            <th scope="col">Designation</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Employee Code</th>
+                            <th scope="col">Religion</th>
+        
+                            @foreach($weeks as $week)
+                                <th scope="col">
+                                    {{date("d M", strtotime($week[0]))}} <br>
+                                    {{date("d M", strtotime($week[1]))}}
+                                </th>
+                            @endforeach
+        
+                            <th scope="col">Total Working Hour</th>
+                            </tr>  
+                        </thead>
+                        <tbody>
+                            @foreach($branches as $branch)
+                                <tr>
+                                    <th class="bg-secondary" colspan="14" scope="row">{{$branch->name}}</th>
+                                </tr>
                                 @foreach ($users as $user)
                                     @if ($user->branch_id == $branch->id)
                                     <tr>
@@ -47,13 +48,11 @@
                                     </tr>
                                     @endif
                                 @endforeach
-                                <tr>
-                            </tbody>
-                        </table> 
-                    </div>
-                </div> <!-- end row -->
-            </div> <!-- end container -->
-        </section>
-        <div class="page-break"></div>
-    @endforeach
+                            @endforeach
+                        </tbody>
+                    </table> 
+                </div>
+            </div> <!-- end row -->
+        </div> <!-- end container -->
+    </section>
 @endsection
