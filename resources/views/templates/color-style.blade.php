3e16fa5dbbe3633f403ed7da87003f9b478610b5
Mobashir Monim <mobashir@techynaf.com>
2018-10-22 20:41:35 +0600

    Handle people entering pound into hex code

--- resources/views/templates/color-style.blade.php
+++ resources/views/templates/color-style.blade.php
@@ -1,13 +1,37 @@
 @foreach($branches as $branch)
-    <style>
-        .{{str_replace(" ", "-", strtolower($branch->name))}}{
-            color: #{{($branch->color)}};
-        }
-        
-        .{{str_replace(" ", "-", strtolower($branch->name)).'b'}}{
-            background-color: #{{($branch->color)}};
-        }
-    </style>
+    @if ($branch->color == null)
+        <style>
+            .{{str_replace(" ", "-", strtolower($branch->name))}}{
+                color: #000000;
+            }
+            
+            .{{str_replace(" ", "-", strtolower($branch->name)).'b'}}{
+                background-color: #000000;
+            }
+        </style>
+    @else
+        @if (substr($branch->color, -7, 1) == '#')
+            <style>
+                .{{str_replace(" ", "-", strtolower($branch->name))}}{
+                    color: {{($branch->color)}};
+                }
+                
+                .{{str_replace(" ", "-", strtolower($branch->name)).'b'}}{
+                    background-color: {{($branch->color)}};
+                }
+            </style>
+        @else
+            <style>
+                .{{str_replace(" ", "-", strtolower($branch->name))}}{
+                    color: #{{($branch->color)}};
+                }
+                
+                .{{str_replace(" ", "-", strtolower($branch->name)).'b'}}{
+                    background-color: #{{($branch->color)}};
+                }
+            </style>
+        @endif
+    @endif
 @endforeach
 
 <style type="text/css" media="print">
