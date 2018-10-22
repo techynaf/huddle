d33d20ccb2c985448df6151f8fa44721892bfcef
Mobashir Monim <mobashir@techynaf.com>
2018-10-22 18:29:35 +0600

    Fix print schedule select branch

--- resources/views/schedule/branch-select.blade.php
+++ resources/views/schedule/branch-select.blade.php
@@ -1,27 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-        <div class="col-3"></div>
-        <h2 class="page-title">Select Branch</h2>
-    </div>
+<br>
     <form action="/schedule/print" method="GET">
         <div class="row">
             <div class="col"></div>
-            <div class="col-6 card-box">
-                <div class="row">
-                    <div class="col-12">
+            <div class="col-6">
+                <div class="card-box">
+                    <div class="card-title py-1 p-2">
+                        <h2>Select Branch</h2>
+                    </div>
+                    <div class="widget-container p-3">
                         <select name="id" class="form-control">
                             <option value="all">All</option>
                             @foreach ($branches as $branch)
                                 <option value="{{$branch->id}}">{{$branch->name}}</option>
                             @endforeach
                         </select>
+                        <br>
+                        <button type="submit" class="btn btn-primary btn-rounded">Submit</button>
+                        <br><br>
                     </div>
-                </div>
-                <br>
-                <div class="row">
-                    <div class="col-12"><button type="submit" class="btn btn-primary btn-rounded">Submit</button></div>
                 </div>
             </div>
             <div class="col"></div>
