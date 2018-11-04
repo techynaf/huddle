509674c0ccc41ccd020f77ca1672ac81fc30f699
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2018-11-04 23:25:58 +0600

    Implement new scheduler design structure

--- resources/views/templates/schedule-default-form.blade.php
+++ resources/views/templates/schedule-default-form.blade.php
@@ -1,37 +1,42 @@
-<label for="start[]" class="">Entry Time</label>
 <div class="row">
     <div class="col-12">
-        <input type="time" name="start[]" class="form-control input-sm">
+        <div class="row">
+            <div class="col-md-6 mx-0 px-0">
+                <input type="time" name="start[]" class="form-control input-sm">
+            </div>
+            <div class="col-md-6 mx-0 px-0">
+                <input type="time" name="end[]" class="form-control input-sm">
+            </div>
+        </div>
     </div>
 </div>
 
+<br>
+
 @if(auth()->user()->roles->first()->name == 'district-manager' || auth()->user()->roles->first()->name == 'super-admin' || auth()->user()->roles->first()->name == 'HR')
-    <label for="entry_b[]" class="col-form-label ">Entry Branch</label>
-    <select name="entry_b[]" class="form-control ">
-        <option value="{{$user->branch->id}}">{{$user->branch->name}}</option>
-        @foreach($branches as $branch)
-            <option value="{{$branch->id}}">{{$branch->name}}</option>
-        @endforeach
-    </select>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-6 mx-0 px-0">
+                    <select name="entry_b[]" class="form-control ">
+                        <option value="{{$user->branch->id}}">{{$user->branch->name}}</option>
+                        @foreach($branches as $branch)
+                            <option value="{{$branch->id}}">{{$branch->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-md-6 mx-0 px-0">
+                    <select name="exit_b[]" class="form-control ">
+                        <option value="{{$user->branch->id}}">{{$user->branch->name}}</option>
+                        @foreach($branches as $branch)
+                            <option value="{{$branch->id}}">{{$branch->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
 @else
     <input type="hidden" value="{{$user->branch->id}}" name="entry_b[]">
-@endif
-
-<label class="">Exit Time</label>
-<div class="row">
-    <div class="col-12">
-        <input type="time" name="end[]" class="form-control input-sm">
-    </div>
-</div>
-
-@if(auth()->user()->roles->first()->name == 'district-manager' || auth()->user()->roles->first()->name == 'super-admin' || auth()->user()->roles->first()->name == 'HR')
-    <label for="exit_b[]" class="col-form-label ">Exit Branch</label>
-    <select name="exit_b[]" class="form-control ">
-        <option value="{{$user->branch->id}}">{{$user->branch->name}}</option>
-        @foreach($branches as $branch)
-            <option value="{{$branch->id}}">{{$branch->name}}</option>
-        @endforeach
-    </select>
-@else
     <input type="hidden" value="{{$user->branch->id}}" name="exit_b[]">
 @endif
--- resources/views/templates/schedule-form.blade.php
+++ resources/views/templates/schedule-form.blade.php
@@ -1,37 +1,42 @@
-<label for="start[]" class="sm">Entry Time</label>
 <div class="row">
     <div class="col-12">
-        <input type="time" name="start[]" class="form-control input-sm" value="{{$schedule->start}}">
+        <div class="row">
+            <div class="col-md-6 mx-0 px-0">
+                <input type="time" name="start[]" class="form-control input-sm" value="{{$schedule->start}}">
+            </div>
+            <div class="col-md-6 mx-0 px-0">
+                <input type="time" name="end[]" class="form-control input-sm" value="{{$schedule->end}}">
+            </div>
+        </div>
     </div>
 </div>
 
+<br>
+
 @if(auth()->user()->roles->first()->name == 'district-manager' || auth()->user()->roles->first()->name == 'super-admin' || auth()->user()->roles->first()->name == 'HR')
-    <label for="entry_b[]" class="col-form-label">Entry Branch</label>
-    <select name="entry_b[]" class="form-control input-sm">
-        <option value="{{$schedule->startingBranch->id}}">{{$schedule->startingBranch->name}}</option>
-        @foreach($branches as $branch)
-            <option value="{{$branch->id}}">{{$branch->name}}</option>
-        @endforeach
-    </select>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-6 mx-0 px-0">
+                    <select name="entry_b[]" class="form-control input-sm">
+                        <option value="{{$schedule->startingBranch->id}}">{{$schedule->startingBranch->name}}</option>
+                        @foreach($branches as $branch)
+                            <option value="{{$branch->id}}">{{$branch->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-md-6 mx-0 px-0">
+                    <select name="exit_b[]" class="form-control input-sm">
+                        <option value="{{$schedule->endingBranch->id}}">{{$schedule->endingBranch->name}}</option>
+                        @foreach($branches as $branch)
+                            <option value="{{$branch->id}}">{{$branch->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
 @else
     <input type="hidden" value="{{$schedule->startingBranch->id}}" name="entry_b[]">
-@endif
-
-<label>Exit Time</label>
-<div class="row">
-    <div class="col-12">
-        <input type="time" name="end[]" class="form-control input-sm" value="{{$schedule->end}}">
-    </div>
-</div>
-
-@if(auth()->user()->roles->first()->name == 'district-manager' || auth()->user()->roles->first()->name == 'super-admin' || auth()->user()->roles->first()->name == 'HR')
-    <label for="exit_b[]" class="col-form-label sm">Exit Branch</label>
-    <select name="exit_b[]" class="form-control input-sm">
-        <option value="{{$schedule->endingBranch->id}}">{{$schedule->endingBranch->name}}</option>
-        @foreach($branches as $branch)
-            <option value="{{$branch->id}}">{{$branch->name}}</option>
-        @endforeach
-    </select>
-@else
     <input type="hidden" value="{{$schedule->endingBranch->id}}" name="exit_b[]">
 @endif
