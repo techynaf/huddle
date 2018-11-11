ab6ab12f11a2bd0f318ef089b482798cceafa78e
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2018-11-11 11:19:35 +0600

    Resolve conflicts

--- resources/views/schedule/scheduler.blade.php
+++ resources/views/schedule/scheduler.blade.php
@@ -56,7 +56,6 @@
                 </div>    
             </form>
 
-<<<<<<< HEAD
             <div class="bg-white scheduler">
                 <div class="header page-title-schedule text-uppercase font-weight-bold">
                     <div class="row pt-3">
@@ -105,45 +104,6 @@
                                             </div>
                                         @else
                                             @if ($schedule->date != $days[$loop->index][1])
-=======
-            <div class="row">
-                <div class="bg-white scheduler">
-                    <div class="header page-title-schedule text-uppercase">
-                        <div class="row pt-3">
-                            <div class="col-md-1 text-center">Name</div>
-                            @foreach($days as $day)
-                                <div class="col-md">
-                                    {{date("D d M", strtotime($day[1]))}}
-                                    
-                                    {{date("Y", strtotime($day[1]))}}
-                                </div>
-                            @endforeach
-                        </div><!-- row -->
-                        <hr>
-                    </div>
-                    
-                     <div class="table">
-                         @foreach($users as $user)
-                            <form action="/scheduler/{{$user->id}}" method="POST">
-                                @csrf
-                                <div class="row">
-                                    <div class="col-md-1 text-center" id="{{$user->id}}">{{$user->name}}
-                                        <button type="submit" class="btn btn-success btn-rounded">Save</button>
-                                    </div>
-                                    @foreach($schedules[$loop->index] as $schedule)
-                                    <input type="hidden" name="date[]" value="{{$days[$loop->index][1]}}">
-                                        <div class="col-md">
-                                            @if (App\Leave::where('user_id', $user->id)->where('start', '<=', $leaveDate->copy()->addDays($loop->index)->format('Y-m-d'))->where('end', '>=', $leaveDate->copy()->addDays($loop->index)->format('Y-m-d'))->where('is_approved', 1)->first() != null)
-                                                <div class="text-center btn btn-outline-danger h-100 w-100">
-                                                    <input type="hidden" value="{{$user->branch->id}}" name="entry_b[]">
-                                                    <input type="hidden" value="{{$user->branch->id}}" name="exit_b[]">
-                                                    <input type="hidden" value="" name="end[]">
-                                                    <input type="hidden" value="" name="start[]">
-                                                    <input type="hidden" name="s_id[]" value="0">
-                                                    <h6>{{App\Leave::where('user_id', $user->id)->where('start', '<=', $leaveDate->copy()->addDays($loop->index)->format('Y-m-d'))->where('end', '>=', $leaveDate->copy()->addDays($loop->index)->format('Y-m-d'))->where('is_approved', 1)->first()->leavetype->name}}</h6>
-                                                </div>
-                                            @elseif ($schedule == 'false')
->>>>>>> caa08ce52d43dda553b2dbc932ddda4be5d80e2b
                                                 <input type="hidden" name="s_id[]" value="0">
                                                 @include('templates.schedule-default-form')
                                             @elseif (is_string($schedule))
